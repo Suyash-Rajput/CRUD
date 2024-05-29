1471c3a40f831a29dd2ddec210cf51ef962ff564
Suyash-bitpack <suyash.singh@bitpack.me>
2024-05-29 11:06:07 +0530

    Second commit

--- app/main.py
+++ app/main.py
@@ -19,15 +19,21 @@ def get_db():
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_item(db=db, item=item)
 
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
 
 @app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
+def read_items(limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, limit=limit)
     return items
 
 
 @app.put("/items/{item_id}", response_model=schemas.Item)
-def update_item(item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+def update_item(item_id: int, item: schemas.ItemUpdate, db: Session = Depends(get_db)):
     db_item = crud.get_item(db, item_id=item_id)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel
 
 class ItemBase(BaseModel):
+    id : int
     name: str
     category: str
     price: float
+
+class ItemBaseItem(BaseModel):
+     name: str
+     category: str
+     price: float
+
+class ItemUpdate(ItemBaseItem):
+     pass
 
 class ItemCreate(ItemBase):
     pass
