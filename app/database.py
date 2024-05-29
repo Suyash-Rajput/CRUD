069322b212f5850e18786ca0cb4d7a55589c5192
Suyash-bitpack <suyash.singh@bitpack.me>
2024-05-29 10:42:49 +0530

    first commit

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 SQL_URL = os.getenv("DATABASE_URL")
-
+print(SQL_URL)
 engine = create_engine(SQL_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+import uvicorn
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
@@ -39,3 +40,7 @@ def delete_item(item_id: int, db: Session = Depends(get_db)):
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return crud.delete_item(db=db, db_item=db_item)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
